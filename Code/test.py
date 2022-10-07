6b2afc3d33f9cf73bd180e6dd769f246802ca4c7
Brianna28 <thomasdraycott88@gmail.com>
2022-10-07 01:09:22 +0100

    Testing Code

--- Code/test.py
+++ Code/test.py
@@ -2,11 +2,12 @@ import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 
-G = nx.barabasi_albert_graph(12,1)
+G = nx.barabasi_albert_graph(10,3)
 print(f'{nx.average_shortest_path_length(G)=}')
-print(f'{nx.sigma(G)=}')
+#print(f'{nx.sigma(G)=}')
 print(f'{nx.degree_histogram(G)=}')
 A =  nx.degree_histogram(G)
+print(nx.to_numpy_array(G))
 print(dict(enumerate(A)))
 
 subax1 = plt.subplot(121)
