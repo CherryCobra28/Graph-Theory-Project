3328b07ea0b0f5774c0e88f50c1b339500e724ff
Brianna28 <thomasdraycott88@gmail.com>
2022-10-13 15:51:56 +0100

    Update datareader.py

--- Code/data/datareader.py
+++ Code/data/datareader.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 
 data = pd.read_csv('.\data.csv',delimiter=',')
-data.drop(columns=data.columns[0],axis = 1, inplace=True)
+
 print(data.tail())
 print(data.dtypes)
-
-death = data[['Everyone die?']]
-print(death)
+print(data.columns)
+death = data[data['Everyone_Dead'] == True]
+print(len(death))
