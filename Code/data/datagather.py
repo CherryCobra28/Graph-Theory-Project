e2a75d41d9bc3e79288f182d4921cda95d875fcf
Brianna28 <thomasdraycott88@gmail.com>
2022-10-16 23:22:57 +0100

    Update datagather.py

--- Code/data/datagather.py
+++ Code/data/datagather.py
@@ -12,21 +12,31 @@ import numpy as np; import pandas as pd; import sirdmodel;import networkx as nx
 #
 #
 
-def data():
+def datacreate():
     virus = [0.6,0.5]
-    g = nx.complete_graph(5)
-    e = [1,2,3,4,5,1,2,3,4,5,1]
-    n = [50,100,150,200,250,300,350,400,450,500]
+    m = 5
+    g = nx.complete_graph(m+1)
+    e = m
+    n = 30
     d = pd.DataFrame()
-    for _ in range(101):
-        for x in n:
-            A = [sirdmodel.main(g,x,e[0],virus[0],virus[1],enable_vis=False)[0],sirdmodel.main(g,x,e[1],virus[0],virus[1],enable_vis=False)[0],sirdmodel.main(g,x,e[2],virus[0],virus[1],enable_vis=False)[0],sirdmodel.main(g,x,e[3],virus[0],virus[1],enable_vis=False)[0],sirdmodel.main(g,x,e[4],virus[0],virus[1],enable_vis=False)[0],sirdmodel.main(g,x,e[5],virus[0],virus[1],enable_vis=False)[0],sirdmodel.main(g,x,e[6],virus[0],virus[1],enable_vis=False)[0],sirdmodel.main(g,x,e[7],virus[0],virus[1],enable_vis=False)[0],sirdmodel.main(g,x,e[7],virus[0],virus[1],enable_vis=False)[0],sirdmodel.main(g,x,e[8],virus[0],virus[1],enable_vis=False)[0],sirdmodel.main(g,x,e[9],virus[0],virus[1],enable_vis=False)[0],sirdmodel.main(g,x,e[10],virus[0],virus[1],enable_vis=False)[0]]
-            for i in A:
-                a = pd.DataFrame.from_dict([i])
-                d = pd.concat([d,a],ignore_index=True)
+    index = 0 
+    for i in range(101):
+        A = sirdmodel.main(g,n,e,virus[0],virus[1],enable_vis='False')[0]
+        
+        
+        a = pd.DataFrame(A,index =[i])
+        d = pd.concat([d,a],ignore_index=True)
+        index += 1
     
-    return d
-            
-    #print(d.head())
-    #d.to_csv('.\data.csv')
-data().to_csv('data.csv',index = False)
+    d.to_csv('data.csv',index = False)
+
+def dataobserve():
+    a = pd.read_csv('data.csv')
+    print(a)
+    
+    
+    
+if __name__ == '__main__':
+    k = input('create or observe')
+    choice = 'data'+k+'()'
+    eval(choice)
