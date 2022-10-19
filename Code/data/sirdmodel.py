f3b2698112d69753adac67b90d478e7b8900c18a
Brianna28 <thomasdraycott88@gmail.com>
2022-10-19 21:02:58 +0100

    Update sirdmodel.py

--- Code/data/sirdmodel.py
+++ Code/data/sirdmodel.py
@@ -8,7 +8,7 @@ import random as rand #Random helps for random numbers
 import matplotlib.pyplot as plt #A library to plot graphs
 from copy import deepcopy #used to compare the starting graph with the end result
 import PySimpleGUI as sg
-
+from betterdiameter import betterdiameter
 
 
 class infection_graph(): #Creates a class based off the grapgh we are going to analyise and sorts important data about said graph
@@ -19,18 +19,18 @@ class infection_graph(): #Creates a class based off the grapgh we are going to a
         self.colours = dict() #Initialises the colour dict that we use to colour nodes in the graph
         self.histogram = dict(enumerate(nx.degree_histogram(network)))#Creates a dictionary where the degree is the key and the frequency of that degree is the value
         self.highestdegree = list(self.histogram)[-1] #Gives the last element of the histogram to give the highest degree
-        self.diameter = nx.diameter(network) #Returns the furthest distance between nodes in the graph
+        self.diameter = betterdiameter(network) #Returns the furthest distance between nodes in the graph
         vertices = list(nx.nodes(self.graph))
         r_number = rand.randrange(0,len(vertices))
         self.infected.add(vertices[r_number]) #Picks a vertex at random to start the infection
         zeros = [0]*len(vertices)
         self.daysinfected = dict(zip(vertices,zeros))#Keeps count of how long each node has been infected
         self.colour() #Colours the nodes
-    def __repr__(self):
-        '''The __repr__ returns readable info about the graph, here it gives the: degrees of each node,
+    def stats(self):
+        '''returns readable info about the graph, here it gives the: degrees of each node,
         the histogram of the degrees, the highest degree in the graph aka the super hubs and the diameter of the graph'''
-        b = f'{self.degrees}\n{self.histogram=}\n{self.highestdegree=}\n{self.diameter=} '
-        return b
+        
+        return {'histogram':self.histogram, 'highest_degree':self.highestdegree,'Diameter':self.diameter} 
     def die_or_recover(self,node,r: float):
         '''This Method runs after a node has been infceted for k days and will attempt to allow the node to recover at p = r
         or die at p = 1-r'''
@@ -68,10 +68,10 @@ class cgraph:
         self.colours = []
         self.histogram = dict(enumerate(nx.degree_histogram(network)))
         self.highestdegree = list(self.histogram)[-1]
-        self.diameter = nx.diameter(network)
+        self.diameter = betterdiameter(network)
         self.colour()
-    def __repr__(self):
-        return f'{self.degrees=}\n {self.histogram=}\n {self.highestdegree=}\n {self.diameter=}'
+    def stats(self):
+        return {'histogram':self.histogram, 'highest_degree':self.highestdegree,'Diameter':self.diameter}
     def colour(self):
         for key in self.degrees:
             if self.degrees[key] == self.highestdegree:
@@ -140,7 +140,7 @@ def main(init_graph: nx.graph,no_nodes: int, edges: int, p_i: float, p_r: float,
                     theres no point showing the final graph as itll just be empty'''
                     f = plt.figure('Staring graph')
                     #subax1 = plt.subplot(121)
-                    nx.draw(origin_network.graph,node_colours = origin_network.colours,with_labels=True)
+                    nx.draw(origin_network.graph,node_color = origin_network.colours,with_labels=True)
                     f.show()
                     input()
             else:
@@ -161,7 +161,7 @@ def main(init_graph: nx.graph,no_nodes: int, edges: int, p_i: float, p_r: float,
                     input()
             
             '''Returns a lot of useful info about the graph'''
-            return {'n':no_nodes,'edges added': edges,'P_i': p_i,'P_r': p_r,'Days_Taken': days_of_the_infcetion, 'Surviors': no_of_surviors,'Everyone_Dead':total_death},repr(origin_network)
+            return {'n':no_nodes,'edges added': edges,'P_i': p_i,'P_r': p_r,'Days_Taken': days_of_the_infcetion, 'Surviors': no_of_surviors,'Everyone_Dead':total_death},origin_network.stats()
                 
             
         #Increments the time the infcetions been going on for
@@ -200,6 +200,7 @@ if __name__ == '__main__':
               [sg.InputText()],
               [sg.Text('P of Recovery')],
               [sg.InputText()],
+              [sg.Text('Choice of seed graph:')],
               [sg.Listbox(values=list_of_graphs,size=(15,5), key='-LIST-', enable_events=True)],
               [sg.Checkbox('Enable Graphs?',default = True, key= '-IN-' )],
               [sg.Submit()],
@@ -230,8 +231,7 @@ if __name__ == '__main__':
     
     tup = main(graph,n,e,p_i,p_r,enable_vis)
     '''Then we print out the results of the infection'''
-    infection_data = tup[0]
-    origin_graph = tup[1]
+    infection_data,origin_graph = tup
     print(infection_data)
     print(origin_graph)
     
