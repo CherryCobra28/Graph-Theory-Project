99a53e31accdc351417d8f46db09a40ed6c58922
Brianna28 <thomasdraycott88@gmail.com>
2022-10-21 14:40:09 +0100

    Create gputest.py

---
+++ Code/data/gputest.py
@@ -0,0 +1,42 @@
+from time import perf_counter
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+from copy import copy
+import cupy as cp
+
+
+def betterdiameter(G):
+    A = nx.to_numpy_matrix(G)
+
+    dimensions = A.shape
+    
+    A += np.identity(dimensions[0],int)
+    A = cp.asarray(A)
+    const_A = copy(A)
+    t=1
+    while True:
+
+        x = cp.count_nonzero(A)
+        #print(A)
+        
+        if x == dimensions[0]**2:
+            return t
+            
+        else:
+            t += 1
+            A = cp.matmul(A,const_A)
+            A = (A>0).astype(cp.uint8)
+            #A = ne.evaluate('A>0').astype(np.uint8)
+
+if __name__ == '__main__':
+    K = nx.barabasi_albert_graph(10,7)
+    
+    
+    t = perf_counter()
+    my = betterdiameter(K)
+    print(f'My algoirthim took {perf_counter() - t},{my=}')
+    t = perf_counter()
+    network = nx.diameter(K)
+    print(f'Netwrokx took {perf_counter()-t},{network=}')
+
