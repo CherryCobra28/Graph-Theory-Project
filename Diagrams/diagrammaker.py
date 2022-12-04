73980460e6789d2fa55acec8c13538a0b851f5ad
Brianna28 <thomasdraycott88@gmail.com>
2022-12-04 23:22:44 +0000

    update

--- Diagrams/diagrammaker.py
+++ Diagrams/diagrammaker.py
@@ -5,19 +5,19 @@ import pathlib as path
 
 
 colours = ['blue', 'blue', 'blue','blue','red']
-G = nx.complete_graph(3)
+G = nx.watts_strogatz_graph(10,6,0.6)
+#nx.draw(G)
+#plt.show()
+#
+#G.add_node(3)    
+#G.add_edge(1,3)
+#G.add_edge(2,3)
+#nx.draw(G)
+#plt.show()
+#G.add_node(4)
+#G.add_edge(0,4)
+#G.add_edge(2,4)
 nx.draw(G)
-plt.show()
-
-G.add_node(3)    
-G.add_edge(1,3)
-G.add_edge(2,3)
-nx.draw(G)
-plt.show()
-G.add_node(4)
-G.add_edge(0,4)
-G.add_edge(2,4)
-nx.draw(G,node_color=colours)
 plt.show()
 #print(nx.average_shortest_path_length(G))
 #G_Degree = dict(nx.degree(G))
