b5b2cd25b00f7aa0a8fd7a1ace9f1c47b0923e12
Brianna28 <thomasdraycott88@gmail.com>
2022-12-20 17:46:58 +0000

    More Refactoring For Stretch G

--- Code/data/sirdmodel.py
+++ Code/data/sirdmodel.py
@@ -11,13 +11,32 @@ selecting one node at random and then at a rate p, will attempt to infect other 
  
 '''
 from abc import ABC, abstractmethod
-from copy import deepcopy #used to compare the starting graph with the end result
+from copy import deepcopy
+ #used to compare the starting graph with the end result
 import random as rand #Random helps for random numbers
-from math import floor
+from math import floor,comb
 import networkx as nx #Adds the networkx package, used to create graph objects
 import matplotlib.pyplot as plt #A library to plot graphs
 import PySimpleGUI as sg
 from betterdiameter import betterdiameter
+
+
+class sird_graph():
+    def barabasi(init_graph: nx.Graph, no_nodes: int, edges: int):
+        try:
+            return nx.barabasi_albert_graph(no_nodes,edges,initial_graph = init_graph)
+        except nx.exception.NetworkXError:
+            print(f'Number of edges must be less that number of nodes, {edges}>{no_nodes}')
+            userpanel()
+        
+    def random_graph(no_nodes: int, Eedges: int):
+        p = Eedges/comb(no_nodes,2)
+        return nx.erdos_renyi_graph(no_nodes,p)
+    def watts():
+        pass
+    def scale_free():
+        pass
+
 
 
 class infection_graph(): 
@@ -25,6 +44,7 @@ class infection_graph():
     def __init__(self,network):
         #bepsi
         self.graph = network #Stores the graph we are studying 
+        self.no_nodes = nx.number_of_nodes(self.graph)
         self.infected = set() #Initalises the empty list
         self.degrees = dict(nx.degree(network)) #returns a dictionary with the nodes as keys and their degree as value
         self.histogram = dict(enumerate(nx.degree_histogram(network)))#Creates a dictionary where the degree is the key and the frequency of that degree is the value
@@ -38,8 +58,6 @@ class infection_graph():
         self.daysinfected = dict(zip(vertices,zeros))#Keeps count of how long each node has been infected
         self.timesrecovered = dict(zip(vertices,zeros))
         self.colours = dict() #Initialises the colour dict that we use to colour nodes in the graph
-        self.starting_colours = self.starting_colours()
-
         self.colour() #Colours the node
     def stats(self) -> dict:
         '''returns readable info about the graph, here it gives the: degrees of each node,
@@ -78,21 +96,12 @@ class infection_graph():
                 self.colours.update({key:'green'})
             else:
                 self.colours.update({key:'blue'})
-    def starting_colours(self) -> list:
-        colour = []
-        hubsize = floor(len(self.histogram)/2)
-        hub = list(self.histogram)[hubsize]
-        for key in self.degrees:
-            if self.degrees[key] == self.highestdegree:
-                colour.append('yellow')
-            elif hub < self.degrees[key] < self.highestdegree :
-                colour.append('green')
-            else:
-                colour.append('blue')
-        return colour
     def PersonalInfectionRates(self) -> dict:
         pass
-        
+    
+    
+
+
     
     
     
@@ -100,8 +109,6 @@ class infection_strat(ABC):
     @abstractmethod
     def infect(infclass: infection_graph, p: float) -> None:
         pass
-    
-    
 class ConstantRateInfection(infection_strat):
     def infect(infclass: infection_graph,p: float) -> None:#function to infect a vertex, p is the probaility of infection use a float 
         spreaders = []
@@ -118,22 +125,34 @@ class ConstantRateInfection(infection_strat):
             else:
                 pass
 class PersonalInfection(infection_strat):
-    def infect(infclass: infection_graph,p: float = 0) -> None:
+    def infect(infclass: infection_graph) -> None:
         pass
 class SkillCheckInfection(infection_strat):
     def infect(infclass: infection_graph,p:float) -> None:
         pass
     
+
+
+    
+    
+    
+    
+def days_infected_checker(infection: infection_graph,p_r: float, fatal_days: int = 10 ) -> None:
+    for node in infection.daysinfected:
+            if node in infection.infected:
+                infection.daysinfected[node] += 1
+                if infection.daysinfected[node] > fatal_days:
+                    
+                    infection.die_or_recover(node,p_r)
+    
     
 
-    
 
 
 
 
 
-
-def main(init_graph: nx.graph,no_nodes: int, edges: int, p_i: float, p_r: float,enable_vis: bool,infection_type: infection_strat = ConstantRateInfection) -> tuple:
+def main(graph: sird_graph,p_i: float, p_r: float,enable_vis: bool,infection_type: infection_strat = ConstantRateInfection) -> tuple:
     '''init_graph: The intial graph we grow from (nx.graph)
        no_nodes: The total number of nodes our graph will have (int)
        edges: The number of edges added for each node of the graph (int)
@@ -142,19 +161,11 @@ def main(init_graph: nx.graph,no_nodes: int, edges: int, p_i: float, p_r: float,
        enable_vis: Display imags of the graphs or not (boolean)
        
     '''
-    
-    
-    
-    
+
     '''G is our barabsi graph which we build off our init graph'''
-    try:
-        G = nx.barabasi_albert_graph(no_nodes,edges,initial_graph = init_graph)
-    except nx.exception.NetworkXError:
-        print(f'Number of edges must be less that number of nodes, {edges}>{no_nodes}')
-        userpanel()
+ 
     
-    infection_network = infection_graph(G) #Creates an instance of the infection_graph with G
-    #origin_network = cgraph(cp_G) #creates a instance of the cgraph class with the copy of G
+    infection_network = infection_graph(graph) #Creates an instance of the infection_graph witnode
     origin_network = deepcopy(infection_network) #Makes a copy of G so we can compare later
     days_of_the_infcetion = 0
     '''For all intensive purposes this for loop will run forever until either all the nodes die or the infection dies out'''
@@ -162,12 +173,13 @@ def main(init_graph: nx.graph,no_nodes: int, edges: int, p_i: float, p_r: float,
         infection_type.infect(infection_network,p_i) #We call the infect func on our graph and we will do this many times
         '''Here we look in daysinfected and increment the time a node has been infected by one then see if any node has been
         infected for more than 10 days if so the node will attempt to recover or die'''
-        for key in infection_network.daysinfected:
-            if key in infection_network.infected:
-                infection_network.daysinfected[key] += 1
-                if infection_network.daysinfected[key] > 10:
-                    
-                    infection_network.die_or_recover(key,p_r)
+        #for node in infection_network.daysinfected:
+        #    if node in infection_network.infected:
+        #        infection_network.daysinfected[node] += 1
+        #        if infection_network.daysinfected[node] > 10:
+        #            
+        #            infection_network.die_or_recover(node,p_r)
+        days_infected_checker(infection_network,p_r)
 
         '''If there is no nodes left infected either everyones dead or everyones recovered'''
         if len(infection_network.infected) == 0:
@@ -180,7 +192,7 @@ def main(init_graph: nx.graph,no_nodes: int, edges: int, p_i: float, p_r: float,
                     theres no point showing the final graph as itll just be empty'''
                     f = plt.figure('Staring graph')
                     #subax1 = plt.subplot(121)
-                    nx.draw(origin_network.graph,node_color = origin_network.starting_colours,with_labels=True)
+                    nx.draw(origin_network.graph,node_color = origin_network.colours.values(),with_labels=True)
                     f.show()
                     input()
             else:
@@ -193,7 +205,7 @@ def main(init_graph: nx.graph,no_nodes: int, edges: int, p_i: float, p_r: float,
                 if enable_vis is True:
                     '''Here we render both the orginal grpah and a graph of all the survivors to compare the devasation or lack there of'''
                     f = plt.figure('Starting Graph')
-                    nx.draw_networkx(origin_network.graph,node_color = origin_network.starting_colours ,with_labels=True)
+                    nx.draw_networkx(origin_network.graph,node_color = origin_network.colours.values() ,with_labels=True)
                     f.show()
                     g = plt.figure('The Surviors')
                     nx.draw(infection_network.graph,node_color = infection_network.colours.values(),with_labels=True)
@@ -201,7 +213,7 @@ def main(init_graph: nx.graph,no_nodes: int, edges: int, p_i: float, p_r: float,
                     input()
             
             '''Returns a lot of useful info about the graph'''
-            return {'n':no_nodes,'edges added': edges,'P_i': p_i,'P_r': p_r,'Days_Taken': days_of_the_infcetion, 'Surviors': no_of_surviors,'Everyone_Dead':total_death},origin_network.stats()
+            return {'n':origin_network.no_nodes,'P_i': p_i,'P_r': p_r,'Days_Taken': days_of_the_infcetion, 'Surviors': no_of_surviors,'Everyone_Dead':total_death},origin_network.stats()
                 
             
         #Increments the time the infcetions been going on for
@@ -224,58 +236,85 @@ def graphchoice(m,choice) -> nx.Graph:
 
 
 
-def userpanel() -> None:
+def userpanel() -> nx.Graph:
     '''Here is where the actual code runs, We ask the user for input for every parameter of the main function'''
     sg.theme('Green')
-    list_of_graphs = ['Wheel','Cycle','Complete','Star','Erdos-Renyi']   
-    layout = [[sg.Text('No of nodes')],
-              [sg.InputText()],
-              [sg.Text('Barabasi Edges')],
-              [sg.InputText()],
-              [sg.Text('P of infection')],
-              [sg.InputText()],
-              [sg.Text('P of Recovery')],
-              [sg.InputText()],
-              [sg.Text('Choice of seed graph:')],
-              [sg.Listbox(values=list_of_graphs,size=(15,5), key='-LIST-', enable_events=True)],
-              [sg.Checkbox('Enable Graphs?',default = True, key= '-IN-' )],
+    list_of_graph_types = ['Barabasi-Albert','Watts-Strogats','Erdos Random','Scale Free']
+    layout = [[sg.Text('Which type of graph would you like to test?')],
+              [sg.Listbox(values=list_of_graph_types, key='-LIST-', enable_events=True)],
               [sg.Submit()],
-              [sg.Cancel()]]
-
-
+              [sg.Cancel()]
+              ]
     window = sg.Window('SIRD Infection Model', layout)
     while True:
 
-        event, values = window.read()
-        if event in (sg.WIN_CLOSED, 'Exit'):
-            quit()
-        elif 'Submit' in event:
-            break
-        
-        
+            event, values = window.read()
+            if event in (sg.WIN_CLOSED, 'Exit'):
+                quit()
+            elif 'Submit' in event:
+                break
     window.close()
-    if values['-IN-'] is  True:
-        enable_vis = True
-    else:
-        enable_vis = False
     try:
-        n,e,p_i,p_r = values[0],values[1],values[2],values[3]
-        n,e,p_i,p_r = int(n),int(e),float(p_i),float(p_r)
-    except ValueError:
-        print('Please input the correct data types')
+        graph_type = values['-LIST-'][0]
+    except IndexError:
         userpanel()
-    #n,e,p_i,p_r = input('Number of Nodes:'),input('Barabasi edges to add:'),input('Probaility of infection:'),input('Probability to Recover:')
-    #enable_vis = input('Show Graphs?:')
-    graph = graphchoice(e,values['-LIST-'][0])
-
-
     
-    '''Then we print out the results of the infection'''
-    infection_data,origin_graph = main(graph,n,e,p_i,p_r,enable_vis)
-    print(infection_data)
-    print(origin_graph)
-     
+    if graph_type == 'Barabasi-Albert':
+        sg.theme('Green')
+        list_of_graphs = ['Wheel','Cycle','Complete','Star','Erdos-Renyi']   
+        layout = [[sg.Text('No of nodes')],
+                  [sg.InputText()],
+                  [sg.Text('Barabasi Edges')],
+                  [sg.InputText()],
+                  [sg.Text('P of infection')],
+                  [sg.InputText()],
+                  [sg.Text('P of Recovery')],
+                  [sg.InputText()],
+                  [sg.Text('Choice of seed graph:')],
+                  [sg.Listbox(values=list_of_graphs,size=(15,5), key='-LIST-', enable_events=True)],
+                  [sg.Checkbox('Enable Graphs?',default = True, key= '-IN-' )],
+                  [sg.Submit()],
+                  [sg.Cancel()]]
 
+
+        window = sg.Window('SIRD Infection Model', layout)
+        while True:
+
+            event, values = window.read()
+            if event in (sg.WIN_CLOSED, 'Exit'):
+                quit()
+            elif 'Submit' in event:
+                break
+            
+            
+        window.close()
+        if values['-IN-'] is  True:
+            enable_vis = True
+        else:
+            enable_vis = False
+        try:
+            n,e,p_i,p_r = values[0],values[1],values[2],values[3]
+            n,e,p_i,p_r = int(n),int(e),float(p_i),float(p_r)
+        except ValueError:
+            print('Please input the correct data types')
+            userpanel()
+     #n,e,p_i,p_r = input('Number of Nodes:'),input('Barabasi edges to add:'),input('Probaility of infection:'),input('Probability to Recover:')
+     #enable_vis = input('Show Graphs?:')
+        graph = graphchoice(e,values['-LIST-'][0])
+        user_graph = sird_graph.barabasi(graph,n,e)
+        infection_data,origin_graph = main(user_graph,p_i,p_r,enable_vis)
+        print(infection_data)
+        print(origin_graph)
+   
+
+    elif graph_type == 'Watts-Strogats':
+        pass
+    elif graph_type == 'Erdos Random':
+        pass
+    elif graph_type == 'Scale Free':
+        pass  
+    #print(origin_graph)
+   
 
 
 
