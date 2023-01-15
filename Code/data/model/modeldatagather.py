21e51f7e9fbc4bd44a30e4ae7f5af909903fb873
Brianna28 <thomasdraycott88@gmail.com>
2023-01-15 21:57:22 +0000

    UPDATES

--- Code/data/model/modeldatagather.py
+++ Code/data/model/modeldatagather.py
@@ -3,12 +3,15 @@ import sirdmodel as sird
 import networkx as nx
 from dict_zip import dict_zip
 from tqdm import tqdm
+import os
+
+
 
 
 SEED_GRAPHS = [nx.fast_gnp_random_graph(10,0.5) for i in range(100)]
 def gather():
     
-    n = [30,50,70,90,110,130,150,170,190,200]#,300,400,500,1000,1500,2000,2500,5000,10000]
+    n = [30,50,70]#,90,110,130,150,170,190,200]#,300,400,500,1000,1500,2000,2500,5000,10000]
     result = []
 
     for N in tqdm(n):
@@ -23,7 +26,10 @@ def gather():
     #print(A)
     D = pd.DataFrame.from_dict(done)
     print(D)
-    D.to_csv('./model.csv')
+    if os.path.exists('model.csv'):
+        D.to_csv('model.csv',index =False,header=False,mode='a')
+    else:
+        D.to_csv('model.csv',index =False)
 def generate(N):
         TEST_GRAPHS = [nx.barabasi_albert_graph(N,5,initial_graph=k) for k in SEED_GRAPHS]
         A,_ = sird.model(TEST_GRAPHS[0],0.5,0.6,graph_type='barabasi')
